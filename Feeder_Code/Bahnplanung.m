17a0db89fb840fa1731d8aa90f888a43f01f8cb7
Philipp Hörauf <dev@phoerauf.de>
2017-02-25 16:57:48 +0100

    defines.h wiederhergestellt

---
+++ Feeder_Code/Bahnplanung.m
@@ -0,0 +1,46 @@
+%Initialisierung der Anfangs und Endbedingung
+x_act=0;    %Startgeschwindigkeit über timervalue anpassen, x_act wird anhand von timervalue berechnet
+v_act=0;
+x_ziel=1000;
+v_ziel=30;
+a_max=5;
+v_max=50; %aktuell nicht verwendet
+timervalue=5.0; %TODO evtl mit v_act kombinieren bzw. daraus berechnen lassen
+%Einheit für Länge = Schritt
+%Einheit für Zeit = Schritt/Sekunde
+
+geschwindigkeits_log = [1:1000]'; %aktuelle Geschwindigkeit bei jedem timer compare match
+timervalue_log = [1:1000]'; %aktueller Wert bei timer compare match
+
+while(x_act ~= x_ziel) %solange Zielpunkt nicht erreicht wurde
+   geschwindigkeits_log(x_act+1 , 1) = 1/timervalue;
+   timervalue_log(x_act+1 , 1) = timervalue;
+   v_act = 1/timervalue;
+   x_diff = x_ziel - x_act; %Weg bis zum Ziel
+   v_diff = v_ziel - v_act;  %Geschwindigkeitsunterschied zur Zielgeschwindigkeit
+   x_B = (v_act^2) / (2 * a_max) - (v_ziel^2) / (2 * a_max)  %Bremsweg
+   if(v_act >= v_ziel)  %Wenn die aktuelle Geschwindigkeit größer als die Zielgeschwindigkeit ist
+       if(x_diff > x_B) %Wenn der Bremsweg noch ausreicht, Beschleunigen TODO evtl x_B+1
+          %Beschleunigen
+          if(v_act >= v_max)
+              %todo evtl. abbremsen
+          else
+          timervalue = (-v_act + sqrt(v_act^2 + 4*a_max))/(2*a_max);
+          end
+
+       else             %Wenn der Bremsweg nicht mehr reicht, Bremsen
+          %Bremsen
+          timervalue = (-v_act + sqrt(v_act^2 - 4*a_max))/(-2*a_max);
+          
+       end              %ggf Totbereich hinzufügen      
+   else
+       %Beschleunigen
+       timervalue = (-v_act + sqrt(v_act^2 + 4*a_max))/(2*a_max);
+   end   
+   x_act = x_act+1;
+end
+subplot(2, 1, 1);
+plot(geschwindigkeits_log)
+subplot(2, 1, 2);
+plot(timervalue_log)
+
--- Feeder_Code/Schrittmotor_fahren.m
+++ Feeder_Code/Schrittmotor_fahren.m
@@ -3,42 +3,40 @@
 
 % Speicher vorallozieren.
 
-aktX = zeros(102);               % Motor steht bei Position 0
-sollX = 1000;           % Motor soll 1000 Schritte machen
-aktV = zeros(102);               % Motor steht
-sollV = 30;             % Maximal 30 Schritte pro Zeiteinheit
-aktA = zeros(102);               % Motor beschleunigt nicht
-aMax = 5;               % Maximal 5 Schritte pro Zeiteinheit mehr als letztes mal
-letzterSchritt = zeros(102);  % Timerwert für minimale Geschwindigkeit
+aktX = zeros(102,1);               % Motor steht bei Position 0
+sollX = 1000;                      % Motor soll 1000 Schritte machen
+aktV = zeros(102,1);               % Motor steht
+sollV = 0;                         % Zielgeschwindigkeit am Ende der Fahrt
+maxV=30;                           % Maximalgeschwindigkeit beim Verfahren
+aktA = zeros(102,1);               % Motor beschleunigt nicht
+aMax = 1e-5;                          % Maximal 5 Schritte pro Zeiteinheit mehr als letztes mal
+letzterSchritt = zeros(102,1);     % Timerwert für minimale Geschwindigkeit
 letzterSchritt(1) = 2^16-1;
-restschritte = zeros(102);       % minimal nötige Schritte bis zum Ziel
-deltaX = zeros(102);
-deltaV = zeros(102);
+restschritte = zeros(102,1);       % minimal nötige Schritte bis zum Ziel
+deltaX = zeros(102,1);
+deltaV = zeros(102,1);
 
-% TODO: speicher vor Schleife allozieren.
+% TODO: Speicher vor Schleife allozieren.
 
-for j=1:1:100
+for j=2:1:100
     i = round(j); % FOR Schleife über FLOAT. WTF.
-    if i == 1
-        i = 2;
-    end
     
-    deltaX(i) = sollX - aktX(i-1);
-	deltaV(i) = sollV - aktV(i-1);
+    deltaX(i) = sollX - aktX(i-1);  %Strecke die noch zu fahren ist
+	deltaV(i) = sollV - aktV(i-1);  
 	
-	
-    if (aktV > 0)
-		restschritte(i) = (deltaV(i).^2) / (2.*aktA(i)) + (sollV .* deltaV(i)) ./ aktA(i);
+    if (aktV(i) > 0)
+        % minimal nötige Schritte zum bremsen berechnen
+		restschritte(i) = (deltaV(i)^2) / (2*aktA(i)) + (sollV * deltaV(i)) / aktA(i);
     end
     
     if (deltaX(i) < restschritte(i) + 1) % bremsen anfangen
-			aktV(i) =  aktV(i-1) - aMax .* letzterSchritt(i-1);
+			aktV(i) =  aktV(i-1) - aMax * letzterSchritt(i-1);
 
-    elseif (aktV < sollV) % beschleunigen
-				aktV(i) = aktV(i-1) + aMax .* letzterSchritt(i-1);
+    elseif (aktV(i) < maxV) % beschleunigen
+				aktV(i) = aktV(i-1) + aMax * letzterSchritt(i-1);
     end
 		% sonst: konstante Fahrtgeschwindigkeit erreicht
 		% Schritt erzeugen: v = 1/t -> t = 1/aktV
-		letzterSchritt(i) = 1 / (2.*aktV(i));
+		letzterSchritt(i) = 1 / (2*aktV(i));
 		aktX(i) = aktX(i-1)+1;
 end
