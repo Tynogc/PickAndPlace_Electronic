6fcf9f0a68662a0345e951571d017708167d27df
Philipp Hörauf <github@phoerauf.de>
2017-03-04 13:35:41 +0100

    code als submodul hinzugefügt

--- Feeder_Code/Bahnplanung.m
+++ Feeder_Code/Bahnplanung.m
@@ -1,13 +1,13 @@
 %Initialisierung der Anfangs und Endbedingung
-x_act=0;    %Startgeschwindigkeit über timervalue anpassen, x_act wird anhand von timervalue berechnet
+x_act=0;    %Startgeschwindigkeit ï¿½ber timervalue anpassen, x_act wird anhand von timervalue berechnet
 v_act=0;
 x_ziel=1000;
 v_ziel=30;
 a_max=5;
 v_max=50; %aktuell nicht verwendet
 timervalue=5.0; %TODO evtl mit v_act kombinieren bzw. daraus berechnen lassen
-%Einheit für Länge = Schritt
-%Einheit für Zeit = Schritt/Sekunde
+%Einheit fï¿½r Lï¿½nge = Schritt
+%Einheit fï¿½r Zeit = Schritt/Sekunde
 
 geschwindigkeits_log = [1:1000]'; %aktuelle Geschwindigkeit bei jedem timer compare match
 timervalue_log = [1:1000]'; %aktueller Wert bei timer compare match
@@ -18,8 +18,8 @@ while(x_act ~= x_ziel) %solange Zielpunkt nicht erreicht wurde
    v_act = 1/timervalue;
    x_diff = x_ziel - x_act; %Weg bis zum Ziel
    v_diff = v_ziel - v_act;  %Geschwindigkeitsunterschied zur Zielgeschwindigkeit
-   x_B = (v_act^2) / (2 * a_max) - (v_ziel^2) / (2 * a_max)  %Bremsweg
-   if(v_act >= v_ziel)  %Wenn die aktuelle Geschwindigkeit größer als die Zielgeschwindigkeit ist
+   x_B = (v_act^2) / (2 * a_max) - (v_ziel^2) / (2 * a_max);  %Bremsweg
+   if(v_act >= v_ziel)  %Wenn die aktuelle Geschwindigkeit grï¿½ï¿½er als die Zielgeschwindigkeit ist
        if(x_diff > x_B) %Wenn der Bremsweg noch ausreicht, Beschleunigen TODO evtl x_B+1
           %Beschleunigen
           if(v_act >= v_max)
@@ -32,7 +32,7 @@ while(x_act ~= x_ziel) %solange Zielpunkt nicht erreicht wurde
           %Bremsen
           timervalue = (-v_act + sqrt(v_act^2 - 4*a_max))/(-2*a_max);
           
-       end              %ggf Totbereich hinzufügen      
+       end              %ggf Totbereich hinzufï¿½gen      
    else
        %Beschleunigen
        timervalue = (-v_act + sqrt(v_act^2 + 4*a_max))/(2*a_max);
