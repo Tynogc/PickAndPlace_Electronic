0b9f69713b6c413a7c45ad183bf2839fb22ac097
Virtex6 <a.schuhmann@umweltbuero.com>
2020-01-26 23:06:39 +0100

    Logging, Argumente hinzugefügt - Handling von SIGINT verbessert

--- LichtDomController/Software/domControl.py
+++ LichtDomController/Software/domControl.py
@@ -1,30 +1,47 @@
-#!/bin/python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 # Programm/Daemon zur IO-Kontrolle des CamDoms
 
-import os
-from time import sleep
 from luma.core.interface.serial import i2c
 from luma.core.render import canvas
-from luma.oled.device import ssd1306, ssd1309, ssd1325, ssd1331, sh1106
-
+from luma.oled.device import ssd1306
+import luma.core.error
 import spidev
-
 import ctypes
 import RPi.GPIO as gpio
-import signal, os
-import SocketServer
+import os
+import socketserver
 import threading
-import Queue
-
+import queue
+import logging
+import logging.handlers
+import argparse
 c_uint8 = ctypes.c_uint8
-
-serial = i2c(port=1, address=0x3C)
-oled = ssd1306(serial, rotate=2)
-speicher = ""
-msg_queue = Queue.Queue()
-
+# Definition of local classes
+class Ledstruct(ctypes.LittleEndianStructure):
+    _fields_ = [("O45R", c_uint8, 1),
+                ("O45G", c_uint8, 1),
+                ("S45R", c_uint8, 1),
+                ("S45G", c_uint8, 1),
+                ("S90R", c_uint8, 1),
+                ("S90G", c_uint8, 1),
+                ("W90R", c_uint8, 1),
+                ("W90G", c_uint8, 1),
+                ("W45R", c_uint8, 1),
+                ("W45G", c_uint8, 1),
+                ("N90R", c_uint8, 1),
+                ("N90G", c_uint8, 1),
+                ("O90R", c_uint8, 1),
+                ("O90G", c_uint8, 1),
+                ("N45R", c_uint8, 1),
+                ("N45G", c_uint8, 1),
+                ("O00R", c_uint8, 1),
+                ("O00G", c_uint8, 1),
+                ("W00R", c_uint8, 1),
+                ("W00G", c_uint8, 1)]
+class Ledunion(ctypes.Union):  _fields_ = [("struktur", Ledstruct), ("asUint8", c_uint8 * 3),
+                                        ("asUint32", ctypes.c_uint32)]
 led_lut = dict({ "O45R": 0,
             "O45G":     1,
             "S45R":     2,
@@ -46,42 +63,41 @@ led_lut = dict({ "O45R": 0,
             "W00R":     18,
             "W00G":     19})
 
+# Init global logging handler to syslog
 
-# SPI
-class ledstruktur(ctypes.LittleEndianStructure):
-    _fields_ = [("O45R", c_uint8, 1),
-                ("O45G", c_uint8, 1),
-                ("S45R", c_uint8, 1),
-                ("S45G", c_uint8, 1),
-                ("S90R", c_uint8, 1),
-                ("S90G", c_uint8, 1),
-                ("W90R", c_uint8, 1),
-                ("W90G", c_uint8, 1),
-                ("W45R", c_uint8, 1),
-                ("W45G", c_uint8, 1),
-                ("N90R", c_uint8, 1),
-                ("N90G", c_uint8, 1),
-                ("O90R", c_uint8, 1),
-                ("O90G", c_uint8, 1),
-                ("N45R", c_uint8, 1),
-                ("N45G", c_uint8, 1),
-                ("O00R", c_uint8, 1),
-                ("O00G", c_uint8, 1),
-                ("W00R", c_uint8, 1),
-                ("W00G", c_uint8, 1)]
+logger = logging.getLogger('sysLogLogger')
+address = '/dev/log'
+if not os.path.exists(address):
+    address = 'localhost', 514
+log_handler = logging.handlers.SysLogHandler(address=address)
+logger.addHandler(log_handler)
+log_handler.setLevel(logging.WARNING)
 
+argparser = argparse.ArgumentParser(
+    description=
+    'TCP Handler for lightdom')
+argparser.add_argument(
+    '-v', dest='verbose', action='store_true', help="Verbose output")
+argparser.add_argument(
+    '-p',
+    dest='port',
+    type=int,
+    default=1337,
+    help='Port of tcp handler')
+argparser.add_argument(
+    '-i',
+    dest='interface',
+    default="0.0.0.0",
+    help='Interface of tcp handler')
+args = argparser.parse_args()
 
-class Flags(ctypes.Union):  _fields_ = [("struktur", ledstruktur), ("asUint8", c_uint8 * 3),
-                                        ("asUint32", ctypes.c_uint32)]
+if args.verbose:
+    log_handler.setLevel(logging.DEBUG)
 
+# Create msq_queue from TCP server threads to main thread
+msg_queue = queue.Queue()
 
-leds = Flags()
-
-
-# leds.struktur.O45G = 1
-
-
-#
+leds = Ledunion()
 
 def readRam():
     with open('/proc/meminfo') as file:
@@ -112,7 +128,7 @@ def readCoreTemp():
     return temperatur
 
 
-def printOled(ip, rom, ram, th, status=0):
+def printOled(oled, ip, rom, ram, th, status=0):
     with canvas(oled) as draw:
         draw.rectangle(oled.bounding_box, outline="white", fill="black")
         if status == 0:
@@ -131,7 +147,7 @@ def printOled(ip, rom, ram, th, status=0):
         draw.text((5, 45), th, fill="white")
 
 
-def controlHW(controlword):
+def controlHW(spi, controlword):
     # Unterscheidung des Testwortes nach Anfangsbuchstaben, dieser ist Eindeutig!
     laenge = len(controlword)
     if laenge == 6:
@@ -141,120 +157,112 @@ def controlHW(controlword):
                 leds.asUint32 |= 1 << led_lut[led_name]
             else:
                 leds.asUint32 &= ~(1 << led_lut[led_name])
-            controlLeds(leds)
+            controlLeds(spi, leds)
 
 
-def disableAllLeds():
+def disableAllLeds(spi):
     # alles aus ist Stumpf 24 Bit Nullen
-    spi.xfer([0, 0, 0])
+    leds.asUint32 = 0
+    controlLeds(spi, leds)
 
 
-def controlLeds(led_struk):
+def controlLeds(spi, led_struk):
     spi.xfer([led_struk.asUint8[2], led_struk.asUint8[1], led_struk.asUint8[0]])
 
 
-def testLeds():
-    for i in range(1, 10):
-        spi.xfer([0x04, 0x00, 0x00])
-        sleep(0.5)
-        spi.xfer([0x04, 0x00, 0x01])
-        sleep(0.5)
-
-
-def controlFan(anaus=0):
-    if anaus == 0:
+def controlFan(enable):
+    if enable is True:
         gpio.output(17, gpio.LOW)
-    elif anaus == 1:
+    else:
         gpio.output(17, gpio.HIGH)
 
 
-class NetworkHandler(SocketServer.StreamRequestHandler):
+class NetworkHandler(socketserver.StreamRequestHandler):
 
     def handle(self):
         # MOTD
-        self.wfile.write("LightDOM V1\r\n")
+        self.wfile.write(bytearray("LightDOM V1\r\n", encoding='ascii'))
         # self.rfile is a file-like object created by the handler;
         # we can now use e.g. readline() instead of raw recv() calls
         while True:
             data = self.rfile.readline().strip()
-            print "Got msg with length", len(data)
+            logger.debug("Got msg with length".format(len(data)))
             if len(data) == 0:
                 break
-            print("{} wrote:".format(self.client_address[0]))
-            hex_string = ':'.join(x.encode('hex') for x in data)
-            print(hex_string)
+            logger.debug("{} wrote:".format(self.client_address[0]))
+            hex_string = data.hex()
+            logger.debug(hex_string)
             if len(data) < 3:
-                print "Msg is too short"
+                logger.warning("Msg is too short")
                 break
             # Prüfen des Längenbytes auf ASCII Zahlenbereich
             if not (0 < int(data[0]) < 10):
-                print "Keine ASCII Zahl (", data[0], ") als Längenangabe Type:", type(data[0])
+                logger.warning("Keine ASCII Zahl {} als Längenangabe Type: {}".format(data[0], type(data[0])))
                 break
-
             if len(data) - 1 != (int(data[0])):
-                print "Ungültige Längenangabe"
+                logger.warning("Ungültige Längenangabe")
                 break
             if data[1] != ',':
-                print "Komma als Trennzeichen zwischen Länge und Nachricht nicht gefunden"
+                logger.warning("Komma als Trennzeichen zwischen Länge und Nachricht nicht gefunden")
                 break
             data = data[2:]
             msg_queue.put(data)
-            # Likewise, self.wfile is a file-like object used to write back
-            # to the client
-    #        self.wfile.write(self.data.upper())
 
 
-def signal_handler(signum, frame):
-    print('Signal handler called with signal', signum)
-    shutdown()
-
-
-def shutdown():
-    disableAllLeds()
+def shutdown(spi):
+    disableAllLeds(spi)
     gpio.output(17, gpio.LOW)
     gpio.cleanup()
     server.shutdown()
     server.server_close()
 
+def getSPIInterface():
+    spi = None
+    try:
+        spi = spidev.SpiDev()
+        spi.open(0, 0)
+        spi.max_speed_hz = 10000000
+        spi.mode = 0
+    except Exception as e:
+        logger.warning("Could not open SPI Interface")
+        exit(-1)
+    return spi
+
+def getOledInterface():
+    oled = None
+    try:
+        serial = i2c(port=1, address=0x3C)
+        oled = ssd1306(serial, rotate=2)
+    except luma.core.error:
+        logger.warning("Could not connect to i2c oled display")
+        exit(-1)
+    return oled
+
 
 if __name__ == '__main__':
-    signal.signal(signal.SIGTERM, signal_handler)
-    signal.signal(signal.SIGABRT, signal_handler)
-    signal.signal(signal.SIGINT, signal_handler)
-    spi = spidev.SpiDev()
-    spi.open(0, 0)
-    spi.max_speed_hz = 10000000
-    spi.mode = 0
+
+    spi_interface = getSPIInterface()
+    oled_interface = getOledInterface()
 
     gpio.setmode(gpio.BCM)
     gpio.setup(17, gpio.OUT, initial=gpio.LOW)
 
-    HOST, PORT = "0.0.0.0", 1337
-
     # Create the server, binding to localhost on port 1337
-    server = SocketServer.ThreadingTCPServer((HOST, PORT), NetworkHandler)
-
-    print "mem:", readRam(), "MB"
-    print "sd :", readSD(), "MB"
-    print "IP: ", readIP()
-    print"Core-Temperatur: ", readCoreTemp()
-    printOled(readIP(), readSD(), readRam(), readCoreTemp(), 1)
+    socketserver.ThreadingTCPServer.allow_reuse_address = True
+    server = socketserver.ThreadingTCPServer((args.interface, args.port), NetworkHandler)
+    printOled(oled_interface, readIP(), readSD(), readRam(), readCoreTemp(), 1)
 
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server_thread = threading.Thread(target=server.serve_forever)
-    server_thread.daemon = True
+    server_thread.daemon = False
     server_thread.start()
     while True:
         try:
-            item = msg_queue.get(block=False, timeout=500)
-            controlHW(item)
-        except Queue.Empty:
-            printOled(readIP(), readSD(), readRam(), readCoreTemp(), 1)
-
-
-    server.shutdown()
-    server.server_close()
-
-    gpio.output(17, gpio.HIGH)
-    disableAllLeds()
+            item = msg_queue.get(block=True, timeout=500)
+            controlHW(spi_interface, item)
+        except queue.Empty:
+            printOled(oled_interface, readIP(), readSD(), readRam(), readCoreTemp(), 1)
+        except KeyboardInterrupt:
+            break
+    shutdown(spi_interface)
